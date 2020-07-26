450eab3f7ca7b1bc40291e06fd0512081a2549a5
Rithik Jain <rithik.jain3006@gmail.com>
2020-07-26 19:03:03 +0530

    Added title in material app

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Qkwick Scan Admin",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Rubik',
