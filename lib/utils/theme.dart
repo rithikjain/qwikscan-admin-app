2742086fe4b5197d244215353ea8d0dd09a102a8
Rithik Jain <rithik.jain3006@gmail.com>
2020-07-26 14:53:19 +0530

    Setup base project

---
+++ lib/utils/theme.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Purple = Color(0xFF6378B3);
+
+const Yellow = Color(0xFFFEB43B);
+
+const Grey = Color(0xFFF3F3F3);
+
+const SmallGreyText = TextStyle(
+  color: Color.fromRGBO(0, 0, 0, 0.5),
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+);
+
+const PurpleHeadingText = TextStyle(
+  color: Purple,
+  fontSize: 36,
+  fontWeight: FontWeight.bold,
+);
+
+const BigHeadingText = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+);
+
+const NormalLightText = TextStyle(
+  fontWeight: FontWeight.w300,
+);
+
+const MediumHeadingText = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+final borderRadius8 = BorderRadius.circular(8);
+
+final borderRadius12 = BorderRadius.circular(12);
